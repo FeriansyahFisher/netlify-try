38cf080a3685db8899d445fea1b60ee681ca850a
Feriansyah <feriansav@gmail.com>
2021-09-10 08:14:24 +0700

    edit judul web

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Males Broo
+                    Welcome Broo
                 </div>
 
                 <div class="links">
