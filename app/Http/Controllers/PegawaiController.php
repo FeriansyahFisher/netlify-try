eef61c8ae0fb670d2098bb857e34cf335fab426d
Feriansyah <feriansav@gmail.com>
2021-10-27 07:50:22 +0700

    upload ke git

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
  
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
  
 class PegawaiController extends Controller
 {
-    public function index($nama){
+    public function index(){
  
-    	return $nama;
+    	// mengambil data dari table pegawai
+    	$pegawai = DB::table('pegawai')->get();
+ 
+    	// mengirim data pegawai ke view index
+    	return view('index',['pegawai' => $pegawai]);
  
     }
 
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::get('halo', function(){
 Route::get('dosen','DosenController@index');
 
 Route::get('pegawai/{nama}', 'PegawaiController@index');
+Route::get('pegawai', 'PegawaiController@index');
 
 Route::get('/formulir', 'PegawaiController@formulir');
 Route::post('/formulir/proses', 'PegawaiController@proses');
